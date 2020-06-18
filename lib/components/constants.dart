caa3ec9b0f6373c983821f04b3c83b3a776392f9
Mac <mazharulislame.cse@gmail.com>
2020-06-19 05:15:09 +0600

    // add text style.

---
+++ lib/components/constants.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+const kTextStyleHeading = TextStyle(
+  fontFamily: 'Gotham',
+  fontSize: 27,
+  color: const Color(0xff000000),
+  fontWeight: FontWeight.w700,
+  height: 0.7037037037037037,
+);
+
+const kTextStyleDescription = TextStyle(
+  fontFamily: 'Gotham',
+  fontSize: 15,
+  color: const Color(0xff000000),
+  letterSpacing: 0.24,
+  fontWeight: FontWeight.w300,
+  height: 1.9333333333333333,
+);
+
+const kTextStyleNormalButton = TextStyle(
+  fontFamily: 'Gotham',
+  fontSize: 15,
+  color: const Color(0xff4f44ff),
+  fontWeight: FontWeight.w500,
+  height: 1.2666666666666666,
+);
+
+const kTextStyleActiveButton = TextStyle(
+  fontFamily: 'Gotham',
+  fontSize: 15,
+  color: const Color(0xdeffffff),
+  fontWeight: FontWeight.w500,
+  height: 1.2666666666666666,
+);
+
+const kTextStyleLink = TextStyle(
+  fontFamily: 'Gotham',
+  fontSize: 15,
+  color: const Color(0xff4f44ff),
+  fontWeight: FontWeight.w500,
+  height: 1.2666666666666666,
+);
+
+const kTextStyleVerificationNumber = TextStyle(
+  fontFamily: 'Gotham',
+  fontSize: 21,
+  color: const Color(0xff000000),
+  letterSpacing: 0.336,
+  fontWeight: FontWeight.w300,
+  height: 0.9523809523809523,
+);
+
+const kTextStyleWelcomeDescription = TextStyle(
+  fontFamily: 'Gotham',
+  fontSize: 13,
+  color: const Color(0xff1a1a1a),
+  letterSpacing: 1.053,
+  fontWeight: FontWeight.w300,
+  height: 1.9230769230769231,
+);
+
+const kTextStyleListItemNumber = TextStyle(
+  fontFamily: 'Gotham',
+  fontSize: 15,
+  color: const Color(0xffffffff),
+  fontWeight: FontWeight.w700,
+  height: 1.2666666666666666,
+);
+
+const kTextStyleListItemHeading = TextStyle(
+  fontFamily: 'Gotham',
+  fontSize: 14,
+  color: const Color(0xff000000),
+  letterSpacing: 0.224,
+  fontWeight: FontWeight.w700,
+  height: 1.7142857142857142,
+);
+
+const kTextStyleListItemDescription = TextStyle(
+  fontFamily: 'Gotham',
+  fontSize: 13,
+  color: const Color(0xff000000),
+  letterSpacing: 0.20800000000000002,
+  fontWeight: FontWeight.w300,
+  height: 1.8461538461538463,
+);
+
+const kTextStyleListItemBatch = TextStyle(
+  fontFamily: 'Gotham',
+  fontSize: 8,
+  color: const Color(0xffffffff),
+  letterSpacing: 0.128,
+  fontWeight: FontWeight.w700,
+  height: 2.25,
+);
