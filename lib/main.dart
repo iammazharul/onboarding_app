78e8ae5f56dc1f6de04571e469f96e51039077f3
Mac <mazharulislame.cse@gmail.com>
2020-06-19 05:28:44 +0600

    // font link and other error update.

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,15 @@ void main() {
 class OnboardingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Onboarding App'),
-      ),
-      body: MaterialApp(home: Container(
-        child: SizedBox(
-          width: 272.0,
-          child: Text(
-            'Enter your mobile number to \nreceive a verification code',
-            style: TextStyle(
-              fontFamily: 'Gotham',
-              fontSize: 15,
-              color: const Color(0xff000000),
-              letterSpacing: 0.24,
-              fontWeight: FontWeight.w300,
-              height: 1.9333333333333333,
-            ),
-            textAlign: TextAlign.center,
+    return MaterialApp(
+      
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            child: Text('Hi!'),
           ),
         ),
-      ),),
+      ),
     );
   }
 }
